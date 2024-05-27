cbb60c4da5a25ba765c74c192801dc5667998bcd
Sabrina Barros <sabrina7410@hotmail.com.br>
2024-05-27 12:36:30 -0300

    Finalizando app

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input")
 const searchBtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apikey}`);
@@ -13,6 +14,21 @@ async function checkWeather(city){
     document.querySelector(".temp").innerHTML= Math.round(data.main.temp)  + "°C" ;
     document.querySelector(".humidity").innerHTML= data.main.humidity +"%";
     document.querySelector(".wind").innerHTML= data.wind.speed +"km/h";
+
+    if(data.weather[0].main =="Clouds"){
+            weatherIcon.src="assets/images/cloud.png"
+    }else if(data.weather[0].main =="Clear"){
+        weatherIcon.src="assets/images/clear.png"
+    }else if(data.weather[0].main =="Rain"){
+    weatherIcon.src="assets/images/rain.png"
+    }else if(data.weather[0].main =="Drizzle"){
+    weatherIcon.src="assets/images/drizzle.png"
+    }else if(data.weather[0].main =="Mist"){
+        weatherIcon.src="assets/images/mist.png"
+    }else if(data.weather[0].main =="Snow"){
+        weatherIcon.src="assets/images/snow.png"
+    }
+    document.querySelector(".weather").style.display ="block";
 }
 
 searchBtn.addEventListener("click",()=>{
