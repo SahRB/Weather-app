e0a07235e5c3acf87bd5ab3238a57e389cf0d327
Sabrina Barros <sabrina7410@hotmail.com.br>
2024-05-27 12:13:34 -0300

    Integrando API

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 const apikey="e7f0d75e9cd31da0d8988ebe21161a6d";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=italy";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apikey}`);
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
     var data =await response.json();
 
     console.log(data);
-
+    document.querySelector(".city").innerHTML= data.name;
+    document.querySelector(".temp").innerHTML= Math.round(data.main.temp)  + "°C" ;
+    document.querySelector(".humidity").innerHTML= data.main.humidity +"%";
+    document.querySelector(".wind").innerHTML= data.wind.speed +"km/h";
 }
-checkWeather();
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value)
+})
+
